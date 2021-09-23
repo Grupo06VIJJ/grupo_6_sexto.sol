ada197301639e755988ce215e9e47ef7c57e151c
Valeria Yashan <valeriayashan@gmail.com>
2021-09-23 17:23:51 -0300

    Commit message

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express=require('express')
 const path=require('path')
 const app =express();
-app.get('/',(req,res)=>{res.sendFile(path.resolve(__dirname  ,'./views/index.html'))
-});
-app.listen(3000,()=> console.log ('Server corriendo en el puerto 3000'));
+app.use(express.static(path.resolve(__dirname, "public")))
+
+
+app.get("/", (req, res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/index.html"))
+})
+
+app.get("/ofertas", (req, res)=> {
+    res.sendFile(path.resolve(__dirname, "./views/index.html"))
+})
+
+app.listen(3000, () =>{
+    console.log("Server corriendo en el puerto 3000");
+})
