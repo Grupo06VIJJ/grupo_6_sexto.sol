c43e9da0ae9da024381b249e7e3a37bdeac6a797
nachoSosa <satrianoignacio@gmail.com>
2021-09-30 18:14:46 -0300

    las imagenes no se veian

--- app.js
+++ app.js
@@ -1,39 +1,34 @@
-const express=require('express')
-const path=require('path')
-const app =express();
-app.use ('/' , express.static(path.resolve(__dirname ,'./public')));
+const express = require('express')
+const path = require('path')
+const app = express();
+app.use('/', express.static(path.resolve(__dirname, './public')));
 
-
-app.get("/", (req, res)=>{
-    res.sendFile(path.resolve(__dirname, "./views/index.html"))
-})
-
-app.get("/ofertas", (req, res)=> {
-    res.sendFile(path.resolve(__dirname, "./views/index.html"))
-})
-/* Register */
-app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
-});
-app.post('/register', (req,res)=> {
-    res.send('Ya estás registrado');
-});
-/* Login */
-app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
-});
-app.post('/login', (req,res)=>{
-    res.sendFile(__dirname +'/views/index.html');
-});
-/* Shopping-cart */
-app.get("/productCart", (req, res)=> {
-    res.sendFile(path.resolve(__dirname, "./views/productCart.html"))
-})
-
-
-
-
-app.listen(process.env.PORT || 3000,function () {
+app.listen(process.env.PORT || 3000, function() {
     console.log(`Servidor corriendo en puerto 3000`)
 });
 
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/index.html"))
+})
+
+app.get("/ofertas", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "./views/index.html"))
+    })
+    /* Register */
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/views/register.html');
+});
+app.post('/register', (req, res) => {
+    res.send('Ya estás registrado');
+});
+/* Login */
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/views/login.html');
+});
+app.post('/login', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+});
+/* Shopping-cart */
+app.get("/productCart", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/productCart.html"))
+})
