8561612f373d56a57a1927917809f3a27a24d9c0
juanmapiredda <juan_piredda@hotmail.com>
2021-09-30 12:16:09 -0300

    cambio app.js - prueba para imagenes Heroku

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express=require('express')
 const path=require('path')
 const app =express();
-app.use(express.static(path.resolve(__dirname, "public")))
+app.use ('/' , express.static(path.resolve(__dirname ,'./public')));
 
 
 app.get("/", (req, res)=>{
