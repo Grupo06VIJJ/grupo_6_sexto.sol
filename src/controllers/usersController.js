1d54cf9c1996199701f045905f2b8418bd30d9e3
Clonmorfo <salguerojuanfrancisco@gmail.com>
2021-10-13 22:06:40 -0300

    prueba de rutas-heroku 5

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -3,7 +3,7 @@ let usersController = {
     /*login*/
     // pantalla de login
     login: (req, res) => {
-        return res.render('login');
+        return res.render('/users/login');
     },
 
     //pantalla desp de loguearse
