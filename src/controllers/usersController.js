24d0bee359e52b64a980d887ee0dc354b66a60e1
Clonmorfo <salguerojuanfrancisco@gmail.com>
2021-10-14 09:53:23 -0300

    detalles menores

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -3,7 +3,7 @@ let usersController = {
     /*login*/
     // pantalla de login
     login: (req, res) => {
-        res.render('login.ejs');
+        res.render('users/login.ejs');
     },
     //pantalla desp de loguearse
     logged: (req, res) => {
