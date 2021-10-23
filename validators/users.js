a56b5b6a00fadb475c35df99634d330e2a2ef2f1
Valeria Yashan <valeriayashan@gmail.com>
2021-10-23 10:55:48 -0300

    controlador y ruta de carrito, validaciones

---
+++ validators/users.js
@@ -0,0 +1,34 @@
+const {check } = require ('express-validator'); //chequear campos de formulario loggin
+const path = require('path');
+const { user } = require('../../db/users');
+ 
+
+module.exports = {
+    
+    create: [],
+    login: [
+        check('email')
+            .notEmpty().withMessage("Completar el  email").bail()
+            .isEmail().withMessage("Ingresar un email válido"),
+        
+        check('password')
+            .notEmpty().withMessage("Completar el password")
+            .bail()
+
+    ],
+    register: [
+        
+        check('full_name')
+            .notEmpty().withMessage("Completar el nombre").bail()
+            .isLength( {min: 5} ).withMessage("El nombre debe tener como mínimo 5 caracteres"),
+        
+        check('email')
+            .notEmpty().withMessage("Completar el email").bail()
+            .isEmail().withMessage("Ingresar un email válido"),
+        
+        check('password')
+            .notEmpty().withMessage("Debes completar el campo password").bail()
+            .isLength( {min: 8} ).withMessage("La contraseña debe tener 8 caracteres como mínimo"),
+        ]
+        
+}
