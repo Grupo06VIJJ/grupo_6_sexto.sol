2b33206902c7663c4b71d8a43ec2875330a05a7e
Valeriayashan <valeriayashan@gmail.com>
2021-11-28 18:39:05 -0300

    comentando validaciones

--- src/middlewares/validateRegisterMiddleware.js
+++ src/middlewares/validateRegisterMiddleware.js
@@ -1,4 +1,4 @@
-const path = require('path');
+/*const path = require('path');
 const { body } = require('express-validator');
 
 module.exports = [
@@ -23,4 +23,4 @@ module.exports = [
 
 		return true;
 	})
-]
+]*/
