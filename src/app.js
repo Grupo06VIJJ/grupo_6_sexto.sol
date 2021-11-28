05467205922010fa7c9d3c6dd7d0d26a6951223c
nachoSosa <satrianoignacio@gmail.com>
2021-11-28 02:49:25 -0300

    Organización app.js

--- src/app.js
+++ src/app.js
@@ -1,56 +1,43 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const ejs = require('ejs');
 const session = require('express-session');
 const bcrypt = require('bcrypt');
-const methoOverride = require("method-override");
+
+/* formularios */
+const methodOverride = require("method-override");
+
+/* templates */
+app.set('view engine', 'ejs');
+
+/* vistas */
+app.set('views', path.resolve(__dirname, 'views'));
+
+/* formularios y json */
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(methodOverride("_method"))
+
+/* public */
+app.use('/', express.static(path.resolve(__dirname, '../public')));
+
+/* rutas */
 const usersRoutes = require("./routes/usersRoutes.js");
 const productsRoutes = require("./routes/productsRoutes.js");
 const mainRoutes = require("./routes/mainRoutes.js");
-/*const res = require('express/lib/response');*/
 
-app.set('view engine', 'ejs');
-app.set('views', path.resolve(__dirname, 'views'));
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(methoOverride("_method"))
-
-app.use('/', express.static(path.resolve(__dirname, '../public')));
+/* rutas */
 app.use("/users", usersRoutes);
 app.use("/products", productsRoutes);
 app.use("/", mainRoutes);
 
-//hashear contraseña
-/*app.use('/users/login', (req, res)=>{
-    const user=req.body.user;
-    const password=req.body.password;
-    if (user=='admin'&& password ='12345'{
-    let passwordhash= await bcrypt.hash(password,8);
-        res.json({
-            message:"Autenticación exitosa",
-            passwordhash:passwordhash
-    });
-    } else {
-        res.json({
-            message:"Ingrese correctamente sus credenciales"
-    });
-    };
-})
-app.get ('/compare', (req,res)=>{
-    let hashSaved=$2b$10$x3X.nWT.e9Q5JuNJw5Tdxuzv3eUD8TnrdskpqRHYJszd0qnk3wyV.
-});
-let compare= bcrypt.compareSync('12345'.hashSaved)
-if (compare){
-    res.json('ok');
-}else{
-    res.json ('No son iguales');
-}
-});*/
-
+/* error 404 */
 app.use((req, res, next) => {
     res.status(404).render("pagina-no-encontrada")
 });
 
+/* PORT o 3000 (heroku y local) */
 app.listen(process.env.PORT || 3000, function() {
     console.log(`Servidor corriendo en puerto 3000`);
 });
